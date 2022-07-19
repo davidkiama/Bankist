b00508a43786cbb8b651dcef68a85ad6318b219e
davidkiama <davidkiama98@gmail.com>
2022-07-20 00:16:23 +0300

    Pointing base url to dpeloyed API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API = axios.create({ baseURL: "https://murmuring-eyrie-53592.herokuapp.com/" });
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://bankist-backend.herokuapp.com/" });
+// const API = axios.create({ baseURL: "http://localhost:5000/" });
 
 API.interceptors.request.use((req) => {
   const profile = localStorage.getItem("profile");
